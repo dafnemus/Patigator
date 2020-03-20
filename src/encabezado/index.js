90c9add3fcacc944202867a2cdc30da3efd6e8f1
FernandezNatasha <natasha.fernandez.arg@gmail.com>
2020-03-20 16:26:22 -0300

    Termine el componente encabezado

--- src/encabezado/index.js
+++ src/encabezado/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import 'antd/dist/antd.css';
-import { PageHeader } from 'antd';
 import './styles.css';
+import { ReactComponent as Logo } from './img/patigator_logo.svg'
 
 function Encabezado() {
     return (
-        <PageHeader
-            className = "site-page-header"
-            class = "title.important"
-            onBack={() => null}
-            title="Patigator"
-        />
-        
+        <header>
+            <Logo id="logo"/>
+            <h1>Patigator</h1>   
+        </header>
     );
 }
 export default Encabezado;
