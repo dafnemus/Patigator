92149c3bdc153d1c0ad50d28b582a723748c23ee
FernandezNatasha <natasha.fernandez.arg@gmail.com>
2020-03-26 16:29:57 -0300

    Quite los cambios de prueba y deje mis cambios hechos en encabezado, buscador y upload

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import {Row, Col, Layout} from 'antd';
 import BotonUpload from './Components/upload/'
 import Buscador from './Components/search/'
 import Encabezado from "./Components/encabezado/";
-import Navegacion from "./Components/navegacion/";
-import Reproductor from "./Components/reproductor/"
 import 'antd/dist/antd.css';
 import './App.css';
 
@@ -18,21 +16,6 @@ function App() {
         <Col span={4}><Buscador/></Col> 
         <Col span={3}><BotonUpload/></Col>
       </Row>
-      <Layout.Content>
-        <Row gutter={[16,40]} justify="center">
-          <Col span={23}><Navegacion/></Col>
-        </Row>
-        <Row  gutter={[8,8]} justify="center">
-          <Col span={11}><Reproductor/></Col>
-          <Col span={11}><Reproductor/></Col>
-          <Col span={11}><Reproductor/></Col>
-          <Col span={11}><Reproductor/></Col>
-          <Col span={11}><Reproductor/></Col>
-          <Col span={11}><Reproductor/></Col>
-          <Col span={11}><Reproductor/></Col>
-          <Col span={11}><Reproductor/></Col>
-        </Row>
-      </Layout.Content>
     </div >
   );
 }
